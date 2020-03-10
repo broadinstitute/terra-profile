c5655b52cf563accbc4aeacc16c8c296bc0fc869
David Mohs <dgit@mohsinc.com>
2020-03-10 13:54:32 -0400

    Support dev and prod databases

--- src/server.js
+++ src/server.js
@@ -4,10 +4,7 @@ const http = require('http')
 const https = require('https')
 const jwt = require('jsonwebtoken')
 const _ = require('lodash/fp')
-
-let config = undefined
-let crypto = undefined
-let ekvdb = undefined
+const u = require('utils')
 
 function errToObj(err) {
   return _.merge({message: err.message, stack: err.stack.split('\n')}, err)
@@ -75,27 +72,89 @@ async function getGcsObjectData(authorization, gsUrlString) {
   return (await slurpStream(gcsRes)).join('')
 }
 
+const configGcsUrl = 'gs://broad-shibboleth-prod.appspot.com/secrets/secrets.20200310a.json'
+
 async function withConfig(req, res, next) {
-  if (!config) {
-    const authorization = `Bearer ${await fetchAccessToken()}`
-    config = JSON.parse(await getGcsObjectData(authorization, process.env.CONFIG_GCS_URL))
-    const authKey = await (async () => {
-      try {
-        return (await fs.readFile('.authkey', {encoding: 'utf8'})).trim()
-      } catch (e) {
-        return undefined
-      }
-    })()
-    config = {...config, authKey}
-  }
-  if (!crypto) {
-    crypto = require('./crypto')(Buffer.from(config.encryptionKeyBase64, 'base64'))
-  }
-  if (!ekvdb) {
-    ekvdb = require('./encryptedkvdb')(crypto, {password: config.dbPassword})
-  }
+  if (res.locals.config) { return next() }
+  const authorization = `Bearer ${await fetchAccessToken()}`
+  res.locals.config = JSON.parse(await getGcsObjectData(authorization, configGcsUrl))
   next()
 }
+
+function withCrypto(envName) {
+  return (req, res, next) => {
+    if (!res.locals.config) { return next(new Error('Missing configuration')) }
+    const {config} = res.locals
+    const key = Buffer.from(config[envName].encryptionKeyBase64, 'base64')
+    res.locals.crypto = require('./crypto')(key)
+    next()
+  }
+}
+
+function withEkvdb(envName) {
+  const instanceConnectionName = envName === 'dev' ?
+    'broad-dsde-dev:us-central1:thurloe-cloudsql-dev201-9e45a2fc870e72f2' :
+    'broad-dsde-prod:us-central1:thurloe-cloudsql-prod201-905168db8ab5e528'
+  return (req, res, next) => {
+    const {config, crypto} = res.locals
+    if (!config) { return next(new Error('Missing configuration')) }
+    if (!crypto) { return next(new Error('Missing crypto library')) }
+    const dbOpts = {instanceConnectionName, password: config[envName].dbPassword}
+    res.locals.ekvdb = require('./encryptedkvdb')(crypto, dbOpts)
+    next()
+  }
+}
+
+class StructError extends Error {
+  constructor(obj) {
+    super(obj.message)
+    _.forEach(k => {this[k] = obj[k]})(_.keys(obj))
+  }
+}
+
+class ExternalRequestError extends Error {
+  constructor(res) {
+    super(`External request returned status: ${res.statusCode}`)
+    this.res = res
+  }
+}
+
+async function hasPermission(authorizationHeaderValue, permissionName) {
+  const permsCheck = u.httpreq({
+    hostname: 'cloudresourcemanager.googleapis.com',
+    path: `/v1/projects/${process.env.GOOGLE_CLOUD_PROJECT}:testIamPermissions`,
+    method: 'post',
+    headers: {'Authorization': authorizationHeaderValue, 'Content-Type': 'application/json'}
+  })
+  permsCheck.req.write(JSON.stringify({permissions: [permissionName]}))
+  permsCheck.req.end()
+  await permsCheck.resp
+  if (permsCheck.res.statusCode !== 200) { throw new ExternalRequestError(permsCheck.res) }
+  permsCheck.body = await u.consumeStreamp(permsCheck.res)
+  const grantedPerms = JSON.parse(permsCheck.body.toString()).permissions
+  if (_.indexOf(permissionName)(grantedPerms) !== -1) {
+    return [null, true]
+  } else {
+    return [{
+      message: 'missing required permission',
+      requiredPermission: requiredPerm,
+      grantedPermissions: grantedPerms
+    }, false]
+  }
+}
+
+app.post('/.src', async (req, res, next) => {
+  const [err, hasPerm] = await hasPermission(
+    req.headers['authorization'],
+    'appengine.applications.update'
+  )
+  if (hasPerm) {
+    res.locals.isReloadOkay = true
+    return next()
+  } else {
+    res.status(403).send(JSON.stringify(err, null, 2)).end()
+  }
+})
 
 async function withAuth(req, res, next) {
   res.sendAuthError = () => {
@@ -107,20 +166,7 @@ async function withAuth(req, res, next) {
     return next()
   }
   const [type, value] = authHeader.split(/\s+/)
-  if (type.toLowerCase() === 'key') {
-    if (!config.authKey) {
-      req.auth = {error: {
-        message: 'key authorization disabled', type, value, header: authHeader}}
-      return next()
-    }
-    if (value !== config.authKey) {
-      req.auth = {error: {message: 'invalid auth key', type, value, header: authHeader}}
-      return next()
-    } else {
-      req.auth = {key: value}
-      return next()
-    }
-  } else if (type.toLowerCase() === 'bearer') {
+  if (type.toLowerCase() === 'bearer') {
     const gtRes = await httpRequest({
       hostname: 'oauth2.googleapis.com',
       path: '/tokeninfo?access_token='+value,
@@ -145,60 +191,86 @@ app.get('/', (req, res) => {
   res.send(`The time is: ${(new Date()).toISOString()}\n`)
 })
 
-app.post('/.src', withAuth)
-
 function jsToUnixTime(msSinceEpoch) { return msSinceEpoch / 1000 }
 function unixToJsTime(sSinceEpoch) { return sSinceEpoch * 1000 }
 
-app.get('/me', withConfig, withAuth, async (req, res) => {
+async function getMe(req, res) {
   if (!req.auth.google) return res.sendAuthError();
+  const {ekvdb} = res.locals
   const pairs = await ekvdb.getPairs(req.auth.google.sub)
   res.sendJson(ekvdb.toPlain(pairs))
-})
+}
 
-app.post('/shibboleth-token', withConfig, withAuth, async (req, res) => {
-  if (!req.auth.google) return res.sendAuthError();
+app.get('/dev/me', withConfig, withCrypto('dev'), withEkvdb('dev'), withAuth, getMe)
+app.get('/me', withConfig, withCrypto('prod'), withEkvdb('prod'), withAuth, getMe)
 
-  const shibToken = (await slurpStream(req)).join('')
-  let payload = null
+async function getPublicKey(envName) {
+  const path = envName === 'dev' ? '/dev/public-key.pem' : '/public-key.pem'
+  const keyReq = await httpRequest({hostname: 'broad-shibboleth-prod.appspot.com', path})
+  if (keyReq.statusCode !== 200) {
+    throw new StructError({message: 'failed to fetch key for JWT verification', path})
+  }
+  return (await slurpStream(keyReq)).join('')
+}
+
+function parseJwtPayload(jwt) {
   try {
     const [headerJson, payloadJson, signatureBytes] =
-      _.map((x) => Buffer.from(x, 'base64').toString())(shibToken.split('.'))
-    payload = JSON.parse(payloadJson)
+      _.map((x) => Buffer.from(x, 'base64').toString())(jwt.split('.'))
+    return JSON.parse(payloadJson)
   } catch (e) {
-    res.status(400)
-      .sendJson({error: {message: 'failed to parse Shibboleth token', received: shibToken}})
+    throw new StructError({message: 'failed to parse token payload', jwt})
   }
+}
 
-  const keyReq = await httpRequest({
-    hostname: 'broad-shibboleth-prod.appspot.com',
-    path: '/public-key.pem',
-  })
-  if (keyReq.statusCode !== 200) {
-    return res.status(502).sendJson({error: {message: 'failed to fetch key for JWT verification'}})
-  }
-  const publicKeyPem = (await slurpStream(keyReq)).join('')
-  try {
-    jwt.verify(shibToken, publicKeyPem, {algorithms: ['RS256']})
-  } catch (e) {
-    return res.status(400).sendJson({error: {message: 'failed to verify JWT', cause: errToObj(e)}})
-  }
+function postShibbolethToken(envName) {
+  return async (req, res) => {
+    if (!req.auth.google) { return res.sendAuthError() }
 
-  const pairs = await ekvdb.getPairs(req.auth.google.sub)
-  await ekvdb.setValue(req.auth.google.sub, pairs,
-    'linkedNihUsername', payload['eraCommonsUsername'])
-  const thirtyDaysInSeconds = 60 * 60 * 24 * 30
-  const exp = payload['iat'] + thirtyDaysInSeconds
-  await ekvdb.setValue(req.auth.google.sub, pairs,
-    'linkExpireTime', exp.toString())
-  const values = _.pick(['linkedNihUsername', 'lastLinkTime', 'linkExpireTime']
-    )(ekvdb.toPlain(await ekvdb.getPairs(req.auth.google.sub)))
-  values.lastLinkTime = parseInt(values.lastLinkTime)
-  values.lastLinkTimeHuman = (new Date(unixToJsTime(values.lastLinkTime))).toString()
-  values.linkExpireTime = parseInt(values.linkExpireTime)
-  values.linkExpireTimeHuman = (new Date(unixToJsTime(values.linkExpireTime))).toString()
-  res.sendJson(values)
-})
+    const {ekvdb} = res.locals
+    const shibToken = (await slurpStream(req)).join('')
+    const payload = parseJwtPayload(shibToken)
+    const publicKeyPem = await getPublicKey(envName)
+
+    try {
+      jwt.verify(shibToken, publicKeyPem, {algorithms: ['RS256']})
+    } catch (e) {
+      return res.status(400).sendJson({error: {message: 'failed to verify JWT', cause: errToObj(e)}})
+    }
+
+    const sub = req.auth.google.sub
+    const payloadUsername = envName === 'dev' ?
+      payload['fake-era-commons-username'] : payload['eraCommonsUsername']
+    const pairs = await ekvdb.getPairs(sub)
+    await ekvdb.setValue(sub, pairs, 'linkedNihUsername', payloadUsername)
+    const thirtyDaysInSeconds = 60 * 60 * 24 * 30
+    const exp = payload['iat'] + thirtyDaysInSeconds
+    await ekvdb.setValue(sub, pairs, 'linkExpireTime', exp.toString())
+    const values = _.pick(['linkedNihUsername', 'lastLinkTime', 'linkExpireTime']
+      )(ekvdb.toPlain(await ekvdb.getPairs(sub)))
+    values.lastLinkTime = parseInt(values.lastLinkTime)
+    values.lastLinkTimeHuman = (new Date(unixToJsTime(values.lastLinkTime))).toString()
+    values.linkExpireTime = parseInt(values.linkExpireTime)
+    values.linkExpireTimeHuman = (new Date(unixToJsTime(values.linkExpireTime))).toString()
+    res.sendJson(values)
+  }
+}
+
+app.post('/dev/shibboleth-token',
+  withConfig, withCrypto('dev'), withEkvdb('dev'), withAuth,
+  postShibbolethToken('dev')
+)
+app.post('/shibboleth-token',
+  withConfig, withCrypto('prod'), withEkvdb('prod'), withAuth,
+  postShibbolethToken('prod')
+)
+
+// app.get('/repl', withConfig, withCrypto('dev'), withEkvdb('dev'), withAuth, async (req, res) => {
+//   if (!req.auth.google) return res.sendAuthError();
+//   const {ekvdb} = res.locals
+//   const pairs = await ekvdb.getPairs(req.auth.google.sub)
+//   res.sendJson(ekvdb.toPlain(pairs))
+// })
 
 app.use((err, req, res, next) => {
   console.error(err)
